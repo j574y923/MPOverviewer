6b2196933da55d562c594709a3c3fbda1e50f7c0
j574y923 <j574y923@gmail.com>
2017-06-27 01:12:46 -0500

    fix auto scroll to stop, scroll all directions

--- src/mpoverviewer/image_layer/tabcontent/EventHandlerRubberBand.java
+++ src/mpoverviewer/image_layer/tabcontent/EventHandlerRubberBand.java
@@ -2,14 +2,11 @@ package mpoverviewer.image_layer.tabcontent;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
-import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.concurrent.Task;
 import javafx.event.EventHandler;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
@@ -21,7 +18,7 @@ import javafx.util.Duration;
  * the rubber band will adjust. Move to the edges of the pane and the pane will
  * readjust position.
  *
- * @author J
+ * @author j574y923
  */
 public class EventHandlerRubberBand implements EventHandler<MouseEvent> {
 
@@ -35,23 +32,11 @@ public class EventHandlerRubberBand implements EventHandler<MouseEvent> {
      */
     private static final double MARGIN = 4;
 
-    private volatile boolean contFlag;
-    private Task task;
-    private Timeline animation;
+    private volatile boolean autoFlagH;
+    private volatile boolean autoFlagV;
+    private Timeline autoAnimationH;
+    private Timeline autoAnimationV;
 
-    /**
-     *
-     * @param rubberBand is already created
-     */
-//    public EventHandlerRubberBand(RectangleRubberBand rubberBand) {
-//        rubberBand = rubberBand;
-////        keyControlRef = (CompositionPaneSP) rubberBand
-////                .getParent()//pane
-////                .getParent()//group
-////                .getParent()//scrollskin$4 (?)
-////                .getParent()//scrollskin$3 (?)
-////                .getParent();//compositionpane
-//    }
     /**
      *
      * @param child contained in parent, this will be where rubber band is added
@@ -60,26 +45,17 @@ public class EventHandlerRubberBand implements EventHandler<MouseEvent> {
      */
     public EventHandlerRubberBand(Pane child, ScrollPane parent) {
         rubberBand = new RectangleRubberBand();
-//        Pane test = new Pane();
-//        test.getChildren().add(rubberBand);
-//        test.addEventHandler(MouseEvent.ANY, this);
-//        child.getChildren().add(test);
         child.getChildren().add(rubberBand);
         this.scrollPane = parent;
-//        scrollPane.getChildren().add(rubberBand);
-//        this.scrollPane = scrollPane;
-//        keyControlRef = (CompositionPaneSP) pane
-//                .getParent()//group
-//                .getParent()//scrollskin$4 (?)
-//                .getParent()//scrollskin$3 (?)
-//                .getParent();//compositionpane
-
         /* rubber band resize if scrollbars change */
         this.scrollPane.hvalueProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                if (contFlag) {
-                    rubberBand.resizeX(newValue.doubleValue() * 2216 + scrollPane.localToScene(scrollPane.getBoundsInLocal()).getMaxX());
+                if (autoFlagH) {
+                    if(newValue.doubleValue() > oldValue.doubleValue())
+                        rubberBand.resizeX(newValue.doubleValue() * 2216 + scrollPane.localToScene(scrollPane.getBoundsInLocal()).getMaxX());
+                    else
+                        rubberBand.resizeX(newValue.doubleValue() * 2216);//??
                 }
             }
 
@@ -87,7 +63,7 @@ public class EventHandlerRubberBand implements EventHandler<MouseEvent> {
         this.scrollPane.vvalueProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                if (contFlag) {
+                if (autoFlagV) {
                     rubberBand.resizeY(newValue.doubleValue() * 4366 + scrollPane.localToScene(scrollPane.getBoundsInLocal()).getMaxY());
                 }
             }
@@ -97,69 +73,127 @@ public class EventHandlerRubberBand implements EventHandler<MouseEvent> {
 
     @Override
     public void handle(MouseEvent mouseEvent) {
-        contFlag = false;
         if (mouseEvent.getEventType() == MouseEvent.MOUSE_PRESSED) {
             rubberBand.begin(mouseEvent.getX(), mouseEvent.getY());
-//                    System.out.println("BEG"+ mouseEvent.getX());
         } else if (mouseEvent.isPrimaryButtonDown()) {
             rubberBand.resize(mouseEvent.getX(), mouseEvent.getY());
-//                    System.out.println(mouseEvent.getX());
-//            System.out.println(mouseEvent.getSceneX());
             navigatePane(mouseEvent);
         } else if (mouseEvent.getEventType() == MouseEvent.MOUSE_RELEASED) {
             rubberBand.end();
-            if (animation != null) {
-                animation = new Timeline(
-                        new KeyFrame(Duration.seconds(1),
-                                new KeyValue(scrollPane.hvalueProperty(), scrollPane.hvalueProperty().get()),
-                                new KeyValue(scrollPane.vvalueProperty(), scrollPane.vvalueProperty().get())));
-                animation.play();
-            }
+            autoFlagH = false;
+            autoFlagV = false;
         }
     }
 
     private void navigatePane(MouseEvent mouseEvent) {
+        int destX = 0;
+        boolean enableX = false;
         if (mouseEvent.getSceneX() >= scrollPane.localToScene(scrollPane.getBoundsInLocal()).getMaxX() - MARGIN) {
             scrollPane.setHvalue(scrollPane.getHvalue() + 0.01);
+            destX = 1;
+            enableX = true;
         } else if (mouseEvent.getSceneX() <= scrollPane.localToScene(scrollPane.getBoundsInLocal()).getMinX() + MARGIN) {
             scrollPane.setHvalue(scrollPane.getHvalue() - 0.01);
+            destX = 0;
+            enableX = true;
+        } else {
+            autoFlagH = false;
+            enableX = false;
         }
 
+        int destY = 0;
+        boolean enableY = false;
         if (mouseEvent.getSceneY() >= scrollPane.localToScene(scrollPane.getBoundsInLocal()).getMaxY() - MARGIN) {
             scrollPane.setVvalue(scrollPane.getVvalue() + 0.005);
-//            System.out.println("A" + mouseEvent.getSceneY() + "B" + scrollPane.localToScene(scrollPane.getBoundsInLocal()).getMaxY());
+            destY = 1;
+            enableY = true;
         } else if (mouseEvent.getSceneY() <= scrollPane.localToScene(scrollPane.getBoundsInLocal()).getMinY() + MARGIN) {
             scrollPane.setVvalue(scrollPane.getVvalue() - 0.005);
+            destY = 0;
+            enableY = true;
+        } else {
+            autoFlagV = false;
+            enableY = false;
         }
 
-        navPaneContinuous(mouseEvent.getSceneX(), mouseEvent.getSceneY());
+        autoScrollH(enableX, destX);
+        autoScrollV(enableY, destY);
     }
 
-    //Exception in thread "JavaFX Application Thread" java.lang.ArrayIndexOutOfBoundsException this is madness
-    private void navPaneContinuous(double sceneX, double sceneY) {
-        if (sceneX >= scrollPane.localToScene(scrollPane.getBoundsInLocal()).getMaxX() - MARGIN && sceneX < 2216) {
-            if (contFlag) {
-                return;
-            }
-            contFlag = true;
-            /**
-             * Credit to https://stackoverflow.com/questions/41723813/how-to-make-javafx-scrollpane-autoscroll-slowly-to-the-bottom
-             */
-            animation = new Timeline(
-                new KeyFrame(Duration.seconds(2),
-                    new KeyValue(scrollPane.hvalueProperty(), 1),
-                    new KeyValue(scrollPane.vvalueProperty(), 1)));
-            animation.play();
+    private void autoScrollH(boolean enable, int destX) {
+        if (!enable) {
+            return;
         }
-        else {
-//            System.out.println("STOP");
-            if(animation != null){
-                animation = new Timeline(
-                    new KeyFrame(Duration.seconds(1),
-                        new KeyValue(scrollPane.hvalueProperty(), scrollPane.hvalueProperty().get()),
-                        new KeyValue(scrollPane.vvalueProperty(), scrollPane.vvalueProperty().get())));
-                animation.play();
-            }
+        if (autoFlagH) {
+            return;
         }
+        autoFlagH = true;
+        /**
+         * Credit to
+         * https://stackoverflow.com/questions/41723813/how-to-make-javafx-scrollpane-autoscroll-slowly-to-the-bottom
+         */
+        autoAnimationH = new Timeline(
+                new KeyFrame(Duration.seconds(zdh(destX)),
+                        new KeyValue(scrollPane.hvalueProperty(), destX)));
+        autoAnimationH.play();
+
+        Thread test = new Thread() {
+            @Override
+            public void run() {
+                while (autoFlagH) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(EventHandlerRubberBand.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                autoAnimationH.stop();
+            }
+        };
+        test.start();
+    }
+
+    private void autoScrollV(boolean enable, int destY) {        
+        if (!enable) {
+            return;
+        }
+        if (autoFlagV) {
+            return;
+        }
+        autoFlagV = true;
+
+        autoAnimationV = new Timeline(
+                new KeyFrame(Duration.seconds(zdv(destY)),
+                        new KeyValue(scrollPane.vvalueProperty(), destY)));
+        autoAnimationV.play();
+
+        Thread test = new Thread() {
+            @Override
+            public void run() {
+                while (autoFlagV) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(EventHandlerRubberBand.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                autoAnimationV.stop();
+            }
+        };
+        test.start();
+    }
+    
+    private double zdh(int destX){
+        if(destX == 1)
+            return 2 - scrollPane.hvalueProperty().get() * 2;
+        else
+            return scrollPane.hvalueProperty().get() * 2;
+    }    
+    
+    private double zdv(int destY){
+        if(destY == 1)
+            return 2 - scrollPane.hvalueProperty().get() * 2;
+        else
+            return scrollPane.hvalueProperty().get() * 2;
     }
 }
